d3ce2a5347f2fd8046146b4e3a92007baa5b123e
André Belhocine <dredtrake@gmail.com>
2022-01-08 01:57:36 +0100

    basic controls are set, we can close the window

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useCallback } from "react";
 import Canvas from "./components/Canvas";
 import "./App.css";
 
@@ -13,24 +12,10 @@ const appStyle = {
   ...css,
 };
 
-const App = () => {
-  const draw = useCallback((ctx, frameCount) => {
-    ctx.fillStyle = "#CCBBFF";
-    ctx.beginPath();
-    ctx.arc(300, 500, 120, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.beginPath();
-    ctx.fillStyle = "#FF99CC";
-    ctx.fillRect(0, 0, 1 * frameCount < 750 ? frameCount * 1 : 750, 750);
-    ctx.fill();
-    // console.log(frameCount, ctx);
-  }, []);
-
-  return (
-    <div className="App" style={appStyle}>
-      <Canvas draw={draw} style={css} />
-    </div>
-  );
-};
+const App = () => (
+  <div className="App" style={appStyle}>
+    <Canvas style={css} />
+  </div>
+);
 
 export default App;
--- src/hooks/useCanvas.js
+++ src/hooks/useCanvas.js
@@ -1,37 +1,95 @@
-import { useCallback, useRef, useEffect } from "react";
+import { useCallback, useRef, useEffect, useState } from "react";
 import { resizeCanvas } from "../utils/resizeCanvas";
 
-const useCanvas = (draw, options = {}) => {
+const [width, height] = [750, 750];
+const useCanvas = (options = {}) => {
+  const dir = {
+    u: 0,
+    d: 0,
+    l: 0,
+    r: 0,
+  };
+
+  const [move, setMove] = useState((p) => {
+    console.log(p);
+    return {
+      ...dir,
+      ...(p && { [p]: dir[p] + 1 }),
+    };
+  });
+
   const canvasRef = useRef(null);
+
+  const draw = useCallback(
+    (ctx, frameCount) => {
+      ctx.fillStyle = "#CCBBFF";
+      ctx.beginPath();
+      ctx.arc(300, 500, 120, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.beginPath();
+      ctx.fillStyle = "#FF99CC"; // left
+      ctx.fillRect(0, 0, move.l < width ? move.l : width, height);
+      ctx.fill();
+      ctx.beginPath();
+      ctx.fillStyle = "#33FFCC"; // right
+      ctx.fillRect(
+        width - move.r,
+        0,
+        move.r < width - move.l ? move.r : width,
+        height
+      );
+      ctx.fill();
+      ctx.beginPath();
+      ctx.fillStyle = "#CCFF99"; // top
+      ctx.fillRect(0, 0, width, move.u < height ? move.u : height);
+      ctx.fill();
+      ctx.beginPath();
+      ctx.fillStyle = "#CC2299"; // bottom
+      ctx.fillRect(
+        0,
+        height - move.d,
+        width,
+        move.d < height ? move.d : height
+      );
+      ctx.fill();
+    },
+    [move]
+  );
 
   const onKeyDown = useCallback((evt) => {
     const { key } = evt;
     if (key === "ArrowUp") {
-      console.log("up");
+      setMove((prevState) => ({ ...prevState, d: prevState.d + 1 }));
     } else if (key === "ArrowDown") {
-      console.log("down");
+      setMove((prevState) => ({ ...prevState, u: prevState.u + 1 }));
     } else if (key === "ArrowLeft") {
-      console.log("left");
+      setMove((prevState) => ({ ...prevState, r: prevState.r + 1 }));
     } else if (key === "ArrowRight") {
-      console.log("right");
+      setMove((prevState) => ({ ...prevState, l: prevState.l + 1 }));
     }
   }, []);
+
+  console.log(move);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext(options.context || "2d");
     let frameCount = 0;
     let animationFrameId;
+
     resizeCanvas(canvas);
 
     const render = () => {
+      //   console.log("framecount", frameCount);
       frameCount++;
       draw(context, frameCount);
       animationFrameId = window.requestAnimationFrame(render);
     };
+
     render();
 
     return () => {
+      console.log("unmount");
       window.cancelAnimationFrame(animationFrameId);
     };
   }, [draw, options]);
